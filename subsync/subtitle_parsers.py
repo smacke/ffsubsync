AakashOfficial/subsync

add subtitle transformer that scales times

subsync/subtitle_parsers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import numbers
 import re
 import sys
 from datetime import timedelta
@@ -128,6 +129,26 @@ class SrtOffseter(_SrtMixin, TransformerMixin):
                                             end=sub.end + self.td_seconds,
                                             content=sub.content))
         self.subs_ = SrtSubtitles(offset_subs, encoding=subs.encoding)
+        return self
+
+    def transform(self, *_):
+        return self.subs_
+
+
+class SrtScaler(_SrtMixin, TransformerMixin):
+    def __init__(self, scale_factor):
+        super(_SrtMixin, self).__init__()
+        assert isinstance(scale_factor, numbers.Number)
+        self.scale_factor = scale_factor
+
+    def fit(self, subs, *_):
+        scaled_subs = []
+        for sub in subs:
+            scaled_subs.append(srt.Subtitle(index=sub.index,
+                                            start=sub.start * self.scale_factor,
+                                            end=sub.end * self.scale_factor,
+                                            content=sub.content))
+        self.subs_ = SrtSubtitles(scaled_subs, encoding=subs.encoding)
         return self
 
     def transform(self, *_):

smacke/ffsubsync

subsync/subtitle_parsers.py
@@ -2,6 +2,8 @@
 import copy
 from datetime import timedelta
 import logging
+import numbers
+import re
 import six
 import sys
 
@@ -209,6 +211,29 @@ class SubtitleOffseter(_SubsMixin, TransformerMixin):
 
     def fit(self, subs, *_):
         self.subs_ = subs.offset(self.td_seconds)
+        return self
+
+    def transform(self, *_):
+        return self.subs_
+
+
+class SubtitleScaler(_SubsMixin, TransformerMixin):
+    def __init__(self, scale_factor):
+        super(_SubsMixin, self).__init__()
+        assert isinstance(scale_factor, numbers.Number)
+        self.scale_factor = scale_factor
+
+    def fit(self, subs, *_):
+        scaled_subs = []
+        for sub in subs:
+            scaled_subs.append(
+                GenericSubtitle(
+                    sub.start * self.scale_factor,
+                    sub.end * self.scale_factor,
+                    sub.inner
+                )
+            )
+        self.subs_ = GenericSubtitlesFile(scaled_subs, format='srt', encoding=subs.encoding)
         return self
 
     def transform(self, *_):