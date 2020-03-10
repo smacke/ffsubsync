a55423c198060c0fef42a0e67fa3ea3337ce30c5
Stephen Macke <smacke@illinois.edu>
2020-03-10 12:01:46 -0700

    add e2e test (disabled for now)

--- subsync/test_end_to_end.py
+++ subsync/test_end_to_end.py
@@ -4,18 +4,25 @@ import filecmp
 import os
 import tempfile
 import pytest
+import yaml
 from subsync import subsync
 
 
 def gen_args():
+    def test_path(fname):
+        return os.path.join('data', fname)
+    with open('data/integration-testing-config.yaml', 'r') as f:
+        config = yaml.load(f, yaml.SafeLoader)
     parser = subsync.make_parser()
-    args = parser.parse_args(['data/28_days_later.npz', '-i', 'data/28_days_later.unsynced.srt'])
-    args.truth = 'data/28_days_later.synced.srt'
-    yield args
+    for test in config['sync_tests']:
+        args = parser.parse_args([test_path(test['reference']), '-i', test_path(test['unsynchronized'])])
+        args.truth = test_path(test['synchronized'])
+        yield args
 
 
+@pytest.mark.end_to_end
 @pytest.mark.parametrize('args', gen_args())
-def test_end_to_end(args):
+def test_sync_matches_ground_truth(args):
     with tempfile.TemporaryDirectory() as dirpath:
         args.srtout = os.path.join(dirpath, 'test.srt')
         assert subsync.run(args) == 0
