17f76ae1cce1a8d03fce396d71a4c078ea11b0fa
Stephen Macke <stephen.macke@gmail.com>
2020-06-08 10:58:19 -0700

    only use rich if py version >= 3.6

--- ffsubsync/__init__.py
+++ ffsubsync/__init__.py
@@ -2,17 +2,20 @@
 import logging
 import sys
 
-from rich.console import Console
-from rich.logging import RichHandler
+try:
+    from rich.console import Console
+    from rich.logging import RichHandler
 
-# configure logging here because some other later imported library does it first otherwise
-# TODO: use a fileconfig
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler(console=Console(file=sys.stderr))]
-)
+    # configure logging here because some other later imported library does it first otherwise
+    # TODO: use a fileconfig
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(console=Console(file=sys.stderr))]
+    )
+except ImportError:
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 from .version import __version__  # noqa
 from .ffsubsync import main  # noqa
