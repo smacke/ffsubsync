aec4308848ed7ae23510dcac32f9acd5a4a06b84
Stephen Macke <stephen.macke@gmail.com>
2021-01-02 10:31:07 -0800

    add 3.8 and 3.9 to setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Topic :: Multimedia :: Sound/Audio :: Speech',
     ],
