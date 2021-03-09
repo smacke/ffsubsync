a92b31a4ac47cfbc63471fa6a0c87ab434e6feb8
Stephen Macke <stephen.macke@gmail.com>
2021-03-08 21:01:22 -0800

    drop py2 from setup.py and ci

--- setup.py
+++ setup.py
@@ -48,8 +48,6 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
