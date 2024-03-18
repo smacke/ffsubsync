de75bdbfe846b3376f8c0bcfe2e5e5db82d7ff20
Stephen Macke <stephen.macke@gmail.com>
2024-03-17 22:35:09 -0700

    updates for python 3.12

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
         'Topic :: Multimedia :: Sound/Audio :: Speech',
     ],
